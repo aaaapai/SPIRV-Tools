d3c3e2be4948dca3d7f4aa294a4bfd8039e0b301
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-02-03 11:13:27 +0800

    Update android_ci_build.bash

--- android_ci_build.bash
+++ android_ci_build.bash
@@ -10,8 +10,8 @@ cmake_build () {
   ANDROID_TARGET=$2
   mkdir -p build
   cd build
-  cmake $GITHUB_WORKSPACE -DCMAKE_BUILD_TYPE=Release  -DANDROID_PLATFORM=26 -DPATHFINDER_TARGET=ANDROID_TARGET -DCMAKE_AR=$ANDROID_NDK_LATEST_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-ar -DCMAKE_RANLIB=$ANDROID_NDK_LATEST_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-ranlib -DANDROID_ABI=$ANDROID_ABI -DCMAKE_SYSTEM_NAME=Android -DANDROID_TOOLCHAIN=clang -DANDROID_ARM_MODE=arm -DCMAKE_MAKE_PROGRAM=$ANDROID_NDK_LATEST_HOME/prebuilt/linux-x86_64/bin/make -DCMAKE_SYSTEM_NAME=Android -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_LATEST_HOME/build/cmake/android.toolchain.cmake
-  cmake --build . --config Release --parallel 4
+  cmake $GITHUB_WORKSPACE -DCMAKE_BUILD_TYPE=Release  -DANDROID_PLATFORM=24 -DPATHFINDER_TARGET=ANDROID_TARGET -DCMAKE_AR=$ANDROID_NDK_LATEST_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-ar -DCMAKE_RANLIB=$ANDROID_NDK_LATEST_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-ranlib -DANDROID_ABI=$ANDROID_ABI -DCMAKE_SYSTEM_NAME=Android -DANDROID_TOOLCHAIN=clang -DANDROID_ARM_MODE=arm -DCMAKE_MAKE_PROGRAM=$ANDROID_NDK_LATEST_HOME/prebuilt/linux-x86_64/bin/make -DCMAKE_SYSTEM_NAME=Android -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_LATEST_HOME/build/cmake/android.toolchain.cmake
+  cmake --build . --config Release --parallel 6
   cmake --install . --prefix .
   find ./ -name '*' -execdir ${TOOLCHAIN}/bin/llvm-strip --strip-debug {} \;
 }
